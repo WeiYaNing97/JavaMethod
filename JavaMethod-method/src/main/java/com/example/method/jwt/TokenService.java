6ce59cf85122fe33fa6d14e7d07c51067d3e9d89
WeiYaNing <WeiYaNing97@163.com>
2024-06-26 21:36:06 +0800

    tokenservice 修改

--- JavaMethod-method/src/main/java/com/example/method/jwt/TokenService.java
+++ JavaMethod-method/src/main/java/com/example/method/jwt/TokenService.java
@@ -38,46 +38,4 @@ public class TokenService
     protected static final long MILLIS_MINUTE = 60 * MILLIS_SECOND;
 
     private static final Long MILLIS_MINUTE_TEN = 20 * 60 * 1000L;
-
-    @Autowired
-    private RedisService redisService;
-
-    // 创建 token
-    public  String createToken(String username) {
-        // 使用 Jwts.builder() 方法创建 JWT 的 Builder
-        return Jwts.builder()
-                // 设置 token 的主题为用户名
-                .setSubject(username)
-                // 设置 token 的过期时间为当前时间加上设定的过期时间
-                .setExpiration(new Date(System.currentTimeMillis() + expireTime))
-                // 使用 HS512 算法和密钥对 token 进行签名
-                .signWith(SignatureAlgorithm.HS512, secret)
-                // 将构建好的 token 转换为紧凑的形式并返回
-                .compact();
-    }
-
-    /**
-     * 从令牌中获取数据声明
-     *
-     * @param token 令牌
-     * @return 数据声明
-     */
-    private Claims parseToken(String token)
-    {
-        return Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-
-    // 刷新 token
-    public  String refreshToken(String token) {
-        Claims claims = parseToken(token);
-        claims.setExpiration(new Date(System.currentTimeMillis() + expireTime));
-        return Jwts.builder()
-                .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
-    }
 }
