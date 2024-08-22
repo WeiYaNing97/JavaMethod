5d65b533e1e9647314680da3ca35c8d6299877aa
WeiYaNing97 <WeiYaNing97@163.com>
2024-08-22 11:38:03 +0800

    枚举

---
+++ JavaMethod-method/src/main/java/com/example/method/enumtest/MyEnum.java
@@ -0,0 +1,24 @@
+package com.example.method.enumtest;
+
+public class MyEnum {
+    public static void main(String[] args) {
+        direction d = direction.left;
+        switch (d){
+            case Up:
+                System.out.println("上");
+                break;
+            case down:
+                System.out.println("下");
+                break;
+            case left:
+                System.out.println("左");
+                break;
+            case right:
+                System.out.println("右");
+                break;
+        }
+    }
+}
+enum direction{
+    Up, down, left, right
+}
