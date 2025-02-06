a11dc5202fb43f4844c8eedfec79c8509f6c6d78
WeiYaNing97 <WeiYaNing97@163.com>
2025-02-06 14:29:28 +0800

    同步代码

---
+++ JavaMethod-example/src/main/java/com/example/instance/thread/ThreadWait.java
@@ -0,0 +1,35 @@
+package com.example.instance.thread;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ThreadWait {
+    static Deque<Integer> list = new LinkedList<>();
+
+    public static void main(String[] args) {
+        list.offerLast(1);
+        list.offerLast(2);
+        list.offerLast(3);
+        Thread thread2 = new Thread(new runTest(list),"出栈线程");
+        thread2.start();
+    }
+}
+class runTest implements Runnable {
+
+    private  Deque<Integer> list = null;
+
+    runTest(Deque<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        while (list.size() > 0) {
+            synchronized (list) {
+                System.err.println(Thread.currentThread().getName()+"数量:"+list.pollFirst());
+            }
+        }
+        System.err.println(Thread.currentThread().getName()+"数量:"+list.pollFirst());
+    }
+}
--- JavaMethod-test/src/main/java/com/example/test/Test.java
+++ JavaMethod-test/src/main/java/com/example/test/Test.java
@@ -2,15 +2,5 @@ package com.example.test;
 
 public class Test {
     public static void main(String[] args) {
-        Test test = new Test();
-        test.getTest();
-    }
-
-    public void getTest() {
-        get();
-    }
-
-    public void get() {
-        System.out.println("test");
     }
 }
